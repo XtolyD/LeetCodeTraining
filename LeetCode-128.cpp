1a3bd2cbccdc1fd3d8854a3b85f4cda676d83d59
XtolyD <xtolyd@qq.com>
2023-12-04 17:51:32 +0800

    12-04 17:51 push

--- LeetCode-128.cpp
+++ LeetCode-128.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 
 using namespace std;
 
@@ -8,14 +10,16 @@ using namespace std;
     请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
 */
 
-// 内存换时间
+
 class Solution
 {
 public:
     int longestConsecutive(vector<int> &nums)
     {
+
         int maxLen = 0;            // 初始化结果
         int curLen = 0;            // 当前长度
+        /*  方法一 内存换时间
         int* arr = new int[2000000007];
         for (int i = 0; i < 2000000007; i++)
             arr[i] = 0;
@@ -31,6 +35,41 @@ public:
                 maxLen = maxLen > curLen ? maxLen : curLen;
                 curLen = 0;
             }
+        }
+        */
+
+
+        // 用哈希表存储每个端点值对应连续区间的长度
+        // 若数已在哈希表中：跳过不做处理
+        // 若是新数加入：
+        // 取出其左右相邻数已有的连续区间长度 left 和 right
+        // 计算当前数的区间长度为：cur_length = left + right + 1
+        // 根据 cur_length 更新最大长度 max_length 的值
+        unordered_map<int, int> mp;
+        for (int num : nums)
+        {
+            // 如果num已经存在，就跳过
+            if (mp.count(num)) continue;
+            
+            // 如果num不存在，就计算其左右相邻数已有的连续区间长度 left 和 right
+            int left = 0;
+            if(mp.count(num - 1))   // 如果num-1存在，就取出其连续区间长度
+                left = mp[num - 1];
+            
+            int right = 0;
+            if(mp.count(num + 1))   // 如果num+1存在，就取出其连续区间长度
+                right = mp[num + 1];
+
+            // 计算当前数的区间长度为：cur_length = left + right + 1
+            curLen = left + right + 1;
+            
+            // 根据 cur_length 更新最大长度 max_length 的值
+            maxLen = maxLen > curLen ? maxLen : curLen;
+            
+            // 更新num的左右端点的长度
+            mp[num - left] = curLen;    // num-left是左端点
+            mp[num + right] = curLen;   // num+right是右端点
+            mp[num] = curLen;           // 当前数也要更新，防止重复计算
         }
         return maxLen;
     }
--- LeetCode-49.cpp
+++ LeetCode-49.cpp
@@ -13,7 +13,11 @@ class Solution
 {
 public:
     unordered_map<char, int> chr_mp = {
-        {'a', 2}, {'b', 3}, {'c', 5}, {'d', 7}, {'e', 11}, {'f', 13}, {'g', 17}, {'h', 19}, {'i', 23}, {'j', 29}, {'k', 31}, {'l', 37}, {'m', 41}, {'n', 43}, {'o', 47}, {'p', 53}, {'q', 59}, {'r', 61}, {'s', 67}, {'t', 71}, {'u', 73}, {'v', 79}, {'w', 83}, {'x', 89}, {'y', 97}, {'z', 101}};
+        {'a', 2}, {'b', 3}, {'c', 5}, {'d', 7}, {'e', 11}, {'f', 13}, {'g', 17}, 
+        {'h', 19}, {'i', 23}, {'j', 29}, {'k', 31}, {'l', 37}, {'m', 41}, {'n', 43}, 
+        {'o', 47}, {'p', 53}, {'q', 59}, {'r', 61}, {'s', 67}, {'t', 71}, 
+        {'u', 73}, {'v', 79}, {'w', 83}, {'x', 89}, {'y', 97}, {'z', 101}
+    };
 
     long int get_Key(string s)
     {
@@ -53,5 +57,19 @@ public:
 
 int main()
 {
+    Solution s;
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    vector<vector<string>> res = s.groupAnagrams(strs);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << "[";
+        for (int j = 0; j < res[i].size(); j++)
+        {
+            cout << res[i][j];
+            if (j != res[i].size() - 1)
+                cout << ", ";
+        }
+        cout << "]" << endl;
+    }
     return 0;
 }
