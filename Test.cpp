f71d71a4c11f806379fd7109eb012effd69a27b6
XtolyD <xtolyd@qq.com>
2023-12-10 19:59:20 +0800

    Test.cpp Edited

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,23 @@
-#include"stdio.h"
-#include"stdlib.h"
+/* This File Gives includes which used frequently */
+
+#include <bits/stdc++.h> //这是万能头文件
+#include <cstdio>        //使用scanf和printf的头文件
+#include <cstring>       //使用C风格字符串函数的头文件
+#include <algorithm>     //使用算法库的头文件，max, min, swap, sort等
+#include <iostream>      //使用cin,cout的头文件
+#include <string>        //使用string时的头文件
+#include <vector>        //使用vector时的头文件
+#include <stack>         //使用stack时的头文件
+#include <bitset>        //使用bitset时的头文件
+#include <cstdlib>       //使用memset函数的头文件
+#include <cmath>         //数学函数的头文件
+#include <set>           //使用set的头文件
+#include <list>          //使用list的头文件
+#include <deque>         //使用deque的头文件
+#include <map>           //使用map的头文件
 
 int main()
 {
     printf("Hello World!\n");
-    system("pause");
     return 0;
 }
